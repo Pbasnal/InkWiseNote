0cbf7126e8a43ce364208d02e15361d22232a5e2
Pankaj Basnal <pankajbasnal17@gmail.com>
2025-01-09 14:40:52 +0700

    Ux improvement (#38)
    * adding vision api secrets in pipeline
    
    * createing properties file at runtime
    
    * creating the secrets file and adding the steps to release
    
    * correcting folder path
    
    * chaning directory and logging
    
    * touch then list
    
    * printing correct path
    
    * writing to correct path
    
    * logging
    
    ---------
    
    Co-authored-by: Pankaj Basnal <pankaj.basnal@agoda.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,12 @@ file("secrets.properties").takeIf { it.exists() }?.apply {
     FileInputStream(this).use { properties.load(it) }
 }
 
+// Print all properties
+println("Loaded properties:")
+properties.forEach { key, value ->
+    println("$key: $value")
+}
+
 android {
     namespace = "com.originb.inkwisenote"
     compileSdk = 34
