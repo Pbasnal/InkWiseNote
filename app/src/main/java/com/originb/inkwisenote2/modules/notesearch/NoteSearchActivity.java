36ac32313321564786f5ee7039420475fefdfc4e
Pankaj Basnal <pankajbasnal17@gmail.com>
2026-01-20 16:49:20 +0530

    using koin in note search

--- app/src/main/java/com/originb/inkwisenote2/modules/notesearch/NoteSearchActivity.java
+++ app/src/main/java/com/originb/inkwisenote2/modules/notesearch/NoteSearchActivity.java
@@ -1,30 +1,18 @@
 package com.originb.inkwisenote2.modules.notesearch;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import com.originb.inkwisenote2.R;
-import com.originb.inkwisenote2.modules.backgroundjobs.BackgroundOps;
-import com.originb.inkwisenote2.modules.backgroundjobs.Events;
-import com.originb.inkwisenote2.modules.ocr.data.NoteOcrTextDao;
-import com.originb.inkwisenote2.modules.ocr.data.NoteOcrText;
-import com.originb.inkwisenote2.modules.repositories.Repositories;
 import com.originb.inkwisenote2.modules.repositories.SmartNotebook;
-import com.originb.inkwisenote2.modules.repositories.SmartNotebookRepository;
 import com.originb.inkwisenote2.modules.smartnotes.ui.SmartNoteGridAdapter;
-import org.greenrobot.eventbus.EventBus;
-import org.greenrobot.eventbus.Subscribe;
-import org.greenrobot.eventbus.ThreadMode;
+import org.koin.android.compat.ViewModelCompat;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class NoteSearchActivity extends AppCompatActivity {
     private EditText searchInput;
@@ -37,12 +25,8 @@ public class NoteSearchActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_results);
 
-        // 1. Initialize ViewModel with Factory
-        NoteSearchViewModelFactory factory = new NoteSearchViewModelFactory(
-                Repositories.getInstance().getNotesDb().noteOcrTextDao(),
-                Repositories.getInstance().getSmartNotebookRepository()
-        );
-        viewModel = new ViewModelProvider(this, factory).get(NoteSearchViewModel.class);
+        // 1. Initialize ViewModel with Koin DI
+        viewModel = ViewModelCompat.getViewModel(this, NoteSearchViewModel.class);
 
         // 2. Initialize UI Components
         searchInput = findViewById(R.id.searchInput);
